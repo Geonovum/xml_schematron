5eb9773e459879f0286a69a56908494b93f6189d
BertVerheesGeonovum <b.verhees@geonovum.nl>
2020-03-03 14:21:10 +0100

    ant file and bash file better

--- 0.98-3-Validatie-_en_conformiteitsregels_TPOD/src/validate.sh
+++ 0.98-3-Validatie-_en_conformiteitsregels_TPOD/src/validate.sh
@@ -2,27 +2,23 @@
 
 for dir in Test*
 do
-	cd ${dir}
-	for file in *.sch
+	cd Ant
+	echo "${dir}"
+	for schemaFile in ../${dir}/*.sch
 	do
-		position=`expr index "${file}" .sch`
-		filename="${file:0:position-1}"
-		echo "${filename}"
-		java -cp ../saxon9.9.1.5/saxon9he.jar net.sf.saxon.Transform -s:${file} -xsl:../saxon9.9.1.5/iso_svrl_for_xslt2.xsl -o:${filename}.xsl
-		for xmlfile in *.xml
+		echo "${schemaFile}"
+		ant compile -q -DschemaFile="${schemaFile}"
+		for xmlFile in ../${dir}/*.xml
 		do
-		  echo "${xmlfile}"
-		  position=`expr index "${xmlfile}" .sch`
-		  filenamexml="${file:0:position-1}"
-		  output="result_${filenamexml}.xml"
-		  echo "${output}"
-		  java -cp ../saxon9.9.1.5/saxon9he.jar net.sf.saxon.Transform -s:${xmlfile} -xsl:${filename}.xsl -o:${output}
+		  echo "${xmlFile}"
+		  ant validate -q -DxmlFile="${xmlFile}"
 		done
-		for gmlfile in *.gml
+		for gmlFile in ../${dir}/*.gml
 		do
-		  echo "${gmlfile}"
+		  echo "${gmlFile}"
+		  ant validate -q -DxmlFile="${gmlFile}"
 		done
-		rm "${filename}.xsl"
+		ant cleanup -q
 	done
 	cd ..
 done
