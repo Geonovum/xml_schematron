ef220fd8d2f787b7f42161547e0f9710eea76740
BertVerheesGeonovum <b.verhees@geonovum.nl>
2020-08-12 14:28:53 +0200

    1840

---
+++ 1.0_ow-1.0.3_op-Validatie-_en_conformiteitsregels_TPOD/src/Test_TPOD1840/test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/sh
+
+
+         rm -f -r ../Validaties
+        cp -R ../../Validaties .
+        rm -f -r Validaties/Opdracht
+        mkdir Validaties/Opdracht
+        cp ../TestData_OW/*.xml Validaties/Opdracht
+        cp TPOD1840.sch Validaties/validaties.sch
+        cp Besluit.xml Validaties/Opdracht
+        rm Validaties/Opdracht/stcrt-2019-56288-produktie.xml
+        cd Validaties/
+        ./validate.sh
+        rm -f -r ../Validaties
+
